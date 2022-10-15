ebf8ae1ed03bae226d14b2ef27405e641c37419e
Walter Lima <86333045+WalterLimaViana@users.noreply.github.com>
2022-10-14 21:41:59 -0300

    Add more lists functionality

--- src/ExemploList.java
+++ src/ExemploList.java
@@ -76,8 +76,35 @@ public class ExemploList {
         notas.add(10.0);
         notas.add(5.6);
 
-        System.out.println("Imprimir novas notas" + notas);
+        System.out.println("Imprimir novas notas" + notas.toString());
+        
+        System.out.println("Exiba a posição da nota 5.0: " + notas.indexOf(5d));
 
+        System.out.println("Adicione na lista a nota 8.0 na posição 4:");
+
+        notas.add(4,8d);
+        System.out.println(notas);
+
+        System.out.println("Substitua a nota 5.0 pela nota 6.0");
+        notas.set(notas.indexOf(5d), 6.0 );
+        System.out.println(notas);
+
+        System.out.println("Confira se a nota 5.0 está na lista: "+ notas.contains(5d));
+
+        System.out.println("Exiba todas as notas que foram informadas: ");
+        for(Double nota: notas) System.out.println(nota);
+
+        System.out.println("Escolha a terceira nota adicionada: " + notas.get(2));
+        System.out.println(notas.toString());
+
+        System.out.println("Exiba a menor nota: " + Collections.min(notas));
+
+        Iterator<Double> iterator = notas.iterator();
+        Double soma = 0d;
+        while(iterator.hasNext()){
+            Double next = iterator.next();
+            soma += next;
+        } 
 
 
 
